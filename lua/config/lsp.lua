d4e035d2578cbe67592c94899b3d7c4c56f3e352
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2025-12-19 18:05:32 +0100

    Fix conform

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,13 +1,13 @@
-vim.lsp.enable({ 'luals', 'clangd' })
+vim.lsp.enable({ 'luals', 'clangd', })
 
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('my.lsp', {}),
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method('textDocument/implementation') then
-            -- Create a keymap for vim.lsp.buf.implementation ...
-        end
+        -- if client:supports_method('textDocument/implementation') then
+        --     -- Create a keymap for vim.lsp.buf.implementation ...
+        -- end
 
         -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
         if client:supports_method('textDocument/completion') then
@@ -21,11 +21,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         -- Auto-format ("lint") on save.
         -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-        if client:supports_method('textDocument/formatting') then
-            vim.keymap.set('n', '<leader>f', function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 999 })
-            end, { desc = '[F]ormat buffer' })
-        end
+        -- if client:supports_method('textDocument/formatting') then
+        --     vim.keymap.set('n', '<leader>f', function()
+        --         vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 999 })
+        --     end, { desc = '[F]ormat buffer' })
+        -- end
     end,
 })
 
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,5 +13,15 @@ require("conform").setup({
         json = { "prettierd", "prettier", stop_after_first = true },
         yaml = { "prettierd", "prettier", stop_after_first = true },
         docker = { "dockerfmt" },
+        cmake = { "cmake_format", stop_after_first = true }
+    },
+
+    default_format_opts = {
+        lsp_format = "prefer",
     },
 })
+
+
+vim.keymap.set('n', '<leader>f', function()
+    require("conform").format({ timeout_ms = 999 })
+end, { desc = '[F]ormat buffer - conform' })
