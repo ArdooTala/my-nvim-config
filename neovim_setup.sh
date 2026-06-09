272929267e20830c3c6c3143bb006ded8366936e
Ardeshir Talaei <ardeshir@Golnars-MacBook-Air.local>
2026-06-09 23:41:03 +0200

    Change binary path

--- neovim_setup.sh
+++ neovim_setup.sh
@@ -13,8 +13,8 @@ git clone https://github.com/ArdooTala/my-nvim-config.git ~/.config/nvim/
 echo 'export TERM="xterm-256color"' >> ~/.bashrc
 
 # Install Neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim-linux-x86_64
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-rm nvim-linux-x86_64.tar.gz
-ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz
+sudo rm -rf /opt/nvim-linux-arm64
+sudo tar -C /opt -xzf nvim-linux-arm64.tar.gz
+rm nvim-linux-arm64.tar.gz
+ln -s /opt/nvim-linux-arm64/bin/nvim /usr/local/bin/nvim
